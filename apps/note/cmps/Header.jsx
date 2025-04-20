f4ffa3975e28ed3d1ef56413a84165e6076df1b8
Nir Sultan <snufkin62@gmail.com>
2025-04-20 23:04:37 +0300

    Fix note logo path

--- apps/note/cmps/Header.jsx
+++ apps/note/cmps/Header.jsx
@@ -5,7 +5,7 @@ export function Header({ handleSearchBarChange, filterBy }) {
 
   return (
     <header className="header">
-      <img className="logo-keep" src="../../../assets/img/note/icons/asset 0.png" alt="" />
+      <img className="logo-keep" src="assets/img/note/icons/asset 0.png" alt="" />
       <h1>Keep</h1>
       <div className="search-container">
         <MagnifyingGlassIcon className="icon-magnifying-glass" />
